49128f95441dad171d9c81e57e139438d666047c
Anastasia P <nastia-dogf@mail.ru>
2022-03-27 18:45:57 +0300

    Initial Commit

---
+++ Test-app-Mobile-Up-Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-app-Mobile-Up-Gallery
+//
+//  Created by Анастасия Перминова on 27.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
