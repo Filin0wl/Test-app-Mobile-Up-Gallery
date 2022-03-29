0cf668801a463d32c3aa7d5ad368b066b519d47a
Anastasia P <nastia-dogf@mail.ru>
2022-03-29 12:06:55 +0300

    Added page Gallery
    Added view controller, xib and storyboard

---
+++ Test-app-Mobile-Up-Gallery/ Pages/GalleryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GalleryViewController.swift
+//  Test-app-Mobile-Up-Gallery
+//
+//  Created by Анастасия Перминова on 29.03.2022.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
