d668fc22adb2db1307c8d8a458e28aba62b685f3
Anastasia P <nastia-dogf@mail.ru>
2022-03-30 17:13:38 +0300

    Added struct API

---
+++ Test-app-Mobile-Up-Gallery/Services/API.swift
@@ -0,0 +1,17 @@
+//
+//  Api.swift
+//  Test-app-Mobile-Up-Gallery
+//
+//  Created by Анастасия Перминова on 30.03.2022.
+//
+
+import Foundation
+
+struct API
+{
+    static let scheme = "https"
+    static let host = "api.vk.com"
+    static let version = "5.131"
+    
+    static let photo = "/method/photos.get"
+}
