28a0c6994a6df03917f9a27cadf5be485fe6ab27
Anastasia P <nastia-dogf@mail.ru>
2022-03-30 13:49:49 +0300

    Int VKSdk
    Added init to AuthService
    Init VKSdk in AppDelegate and SceneDelegate

--- Test-app-Mobile-Up-Gallery/Services/AuthService.swift
+++ Test-app-Mobile-Up-Gallery/Services/AuthService.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import VKSdkFramework
+
+
+class AuthService: NSObject, VKSdkDelegate, VKSdkUIDelegate {
+    
+    //MARK: - Fields
+    private let appId = "8117667"
+    private let vkSDK: VKSdk
+    
+    override init() {
+        vkSDK = VKSdk.initialize(withAppId: appId)
+        super.init()
+        vkSDK.register(self)
+        vkSDK.uiDelegate = self
+    }
+    
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+        print(#function)
+    }
+    
+    func vkSdkUserAuthorizationFailed() {
+        print(#function)
+    }
+    
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        print(#function)
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print(#function)
+    }
+}
