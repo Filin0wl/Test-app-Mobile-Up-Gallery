7f1a39b40f18e5ba79d0dfd86f0c3894e30368fb
Anastasia P <nastia-dogf@mail.ru>
2022-04-01 01:29:44 +0300

    Init navigation bag on ExpandedPhoto page
    And rename icon

--- Test-app-Mobile-Up-Gallery/ Pages/Expanded photo /ExpandedPhotoViewController.swift
+++ Test-app-Mobile-Up-Gallery/ Pages/Expanded photo /ExpandedPhotoViewController.swift
@@ -3,27 +3,83 @@
 //  Test-app-Mobile-Up-Gallery
 //
 //  Created by Анастасия Перминова on 01.04.2022.
+//  Copyright (c) 2022 ___ORGANIZATIONNAME___. All rights reserved.
 //
 
 import UIKit
 
-class ExpandedPhotoViewController: UIViewController {
+protocol ExpandedPhotoDisplayLogic: AnyObject {
+  func displayData(viewModel: ExpandedPhoto.Model.ViewModel.ViewModelData)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class ExpandedPhotoViewController: UIViewController, ExpandedPhotoDisplayLogic {
 
-        // Do any additional setup after loading the view.
+  var interactor: ExpandedPhotoBusinessLogic?
+  var router: (NSObjectProtocol & ExpandedPhotoRoutingLogic)?
+
+  // MARK: Object lifecycle
+  
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    setup()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setup()
+  }
+  
+  // MARK: Setup
+  
+  private func setup() {
+    let viewController        = self
+    let interactor            = ExpandedPhotoInteractor()
+    let presenter             = ExpandedPhotoPresenter()
+    let router                = ExpandedPhotoRouter()
+    viewController.interactor = interactor
+    viewController.router     = router
+    interactor.presenter      = presenter
+    presenter.viewController  = viewController
+    router.viewController     = viewController
+  }
+  
+  // MARK: Routing
+  
+
+  
+  // MARK: View lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+    
+    func setupTopBar() {
+        
+        let shareIcon = UIImage(named: "ShareIcon")
+        let backIcon = UIImage(named: "BackIcon")
+        
+        self.navigationItem.title = "data"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "SF Pro Display Semibold", size: 18)!]
+        
+        let shareButton = UIBarButtonItem(title: "Share", style: .done, target: self, action: #selector(shareButton))
+        shareButton.image = shareIcon
+        self.navigationItem.rightBarButtonItem = shareButton
+        
+        let backButton = UIBarButtonItem(title: "Back", style: .done, target: self, action: #selector(backButton))
+        backButton.image = backIcon
+        self.navigationItem.rightBarButtonItem = backButton
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func shareButton(){
+        //some code for share
     }
-    */
+    
+    @objc func backButton(){
+        //some code for back
+    }
+  
+  func displayData(viewModel: ExpandedPhoto.Model.ViewModel.ViewModelData) {
 
+  }
+  
 }
