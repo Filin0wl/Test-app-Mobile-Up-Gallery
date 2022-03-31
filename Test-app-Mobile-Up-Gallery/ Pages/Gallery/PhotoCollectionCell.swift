3868b2d3ed3666a7bf7a6f7e71dc634969006fed
Anastasia P <nastia-dogf@mail.ru>
2022-03-31 19:48:53 +0300

    Added PhotoCollectionCell

---
+++ Test-app-Mobile-Up-Gallery/ Pages/Gallery/PhotoCollectionCell.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoCollectionCell.swift
+//  Test-app-Mobile-Up-Gallery
+//
+//  Created by Анастасия Перминова on 31.03.2022.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "PhotoCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
+
+
