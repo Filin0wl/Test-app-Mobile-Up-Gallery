22b659266e89ac95b1da0a6f094b96a7a5907286
Anastasia P <nastia-dogf@mail.ru>
2022-03-31 12:50:23 +0300

    Changed NetworkService
    Added authService
    Added func for get photo from album

--- Test-app-Mobile-Up-Gallery/Services/NetworkService.swift
+++ Test-app-Mobile-Up-Gallery/Services/NetworkService.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+final class NetworkService{
+    
+    private let authService: AuthService
+    
+    init(authService: AuthService = SceneDelegate.shared().authService){
+        self.authService = authService
+    }
+    
+    func getPhoto() {
+        
+        guard let token = authService.token else { return }
+        let params = [API.idOwnerParameter: API.idOwner, API.idAlbumParameter: API.idAlbum]
+        var allParams = params
+        allParams[API.accessTokenParameter] = token
+        allParams[API.versionParameter] = API.version
+        let url = self.url(from: API.photo, params: allParams)
+        
+        print(url)
+    }
+    
+    private func url(from path: String, params: [String: String]) -> URL {
+        var components = URLComponents()
+        components.scheme = API.scheme
+        components.host = API.host
+        components.path = path
+        components.queryItems = params.map{ URLQueryItem(name: $0, value: $1) }
+        
+        return url = components.url!
+    }
+}
