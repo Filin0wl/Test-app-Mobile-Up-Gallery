8f84f6b57e203469006d39c4edf11cfd9c0760ca
Anastasia P <nastia-dogf@mail.ru>
2022-03-31 20:58:53 +0300

    Changed GalleryViewController
    Added navigation bar, collection view and collectionCell view

--- Test-app-Mobile-Up-Gallery/ Pages/Gallery/GalleryViewController.swift
+++ Test-app-Mobile-Up-Gallery/ Pages/Gallery/GalleryViewController.swift
@@ -7,24 +7,111 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
+protocol GalleryDisplayLogic: AnyObject {
+  func displayData(viewModel: Gallery.Model.ViewModel.ViewModelData)
+}
 
+class GalleryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, GalleryDisplayLogic {
+    
+    //MAKR: - Outlets
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    
+    //MARK: - External fileds
+    var interactor: GalleryBusinessLogic?
+    var router: (NSObjectProtocol & GalleryRoutingLogic)?
+    
     //MARK: - Internal fileds
     private let networkService: Networking = NetworkService()
     private let fetcher: DataFetcher = NetworkDataFetcher(networking: NetworkService())
     
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
+    // MARK: Object lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+      super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+      setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+      setup()
+    }
+    
+    // MARK: Setup
+    
+    private func setup() {
+      let viewController        = self
+      let interactor            = GalleryInteractor()
+      let presenter             = GalleryPresenter()
+      let router                = GalleryRouter()
+      viewController.interactor = interactor
+      viewController.router     = router
+      interactor.presenter      = presenter
+      presenter.viewController  = viewController
+      router.viewController     = viewController
+    }
+    
+    // MARK: Routing
+    
+
+    
+    // MARK: View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.setupView()
         view.backgroundColor = .blue
         fetcher.getGetPhotoFromAlbum { (photoResponse) in
             guard let photoResponse = photoResponse else { return }
             photoResponse.items.map({ (photoItem) in
                 print(photoItem.date)
             })
+            
 
         }
+        self.setupView()
         
     }
+    
+    func setupView() {
+        self.setupTopBar()
+        collectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+    }
+    
+    func setupTopBar() {
+        self.navigationItem.title = "Mobile Up Gallery"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "SF Pro Display Semibold", size: 17)!]
+        
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "SF Pro Display Medium", size: 18)!], for: UIControl.State.normal)
+        let loginButton = UIBarButtonItem(title: NSLocalizedString("Выход", comment: "Выход"), style: .plain, target: self, action: nil)
+        loginButton.tintColor = .black
+        
+        self.navigationItem.rightBarButtonItem = loginButton
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath)
+        return cell
+    }
+    
+    func displayData(viewModel: Gallery.Model.ViewModel.ViewModelData) {
+
+    }
+    
 
 }
--- Test-app-Mobile-Up-Gallery/ Pages/Gallery/PhotoCollectionCell.swift
+++ Test-app-Mobile-Up-Gallery/ Pages/Gallery/PhotoCollectionCell.swift
@@ -11,8 +11,23 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "PhotoCollectionViewCell"
     
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.addSubview(imageView)
+        
+        let images = [
+            UIImage(named: "image1"),
+            UIImage(named: "image2"),
+            UIImage(named: "image3"),
+        ].compactMap({ $0 })
+        imageView.image = images.randomElement()
         
     }
     
@@ -22,10 +37,12 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        imageView.frame = contentView.bounds
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        imageView.image = nil
     }
 }
 
